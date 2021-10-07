bfaaeb9d7f14f822efe89cd848ee8d69877dc1c7
kiokoshedy <kiokomusembi1@gmail.com>
2021-10-07 18:35:42 +0300

    pages

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import styled from 'styled-components';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GithubIcon from '@material-ui/icons/GitHub';
+import YoutubeIcon from '@material-ui/icons/YouTube';
+import Particle from '../Components/Particle';
+import { LinkedIn, Twitter } from '@material-ui/icons';
+
+function HomePage() {
+    return (
+        <HomePageStyled>
+            <div className="particle-con">
+                <Particle />
+            </div>
+            <div className="typography">
+                <h1> <span>Shadrack Kioko</span></h1>
+                <p>
+                A passionate, adaptable and committed Software Developer who believes that today’s problems can be solved with technology.
+                I am therefore constantly exploring new technologies to strengthen and expand my skills in software development.
+                </p>
+                <div className="icons">
+                    <a href="https://twitter.com/KiokoKe_" className="icon i-twitter">
+                        <Twitter />
+                    </a>
+                    <a href="https://github.com/kiokoshedy" className="icon i-github">
+                        <GithubIcon />
+                    </a>
+                    <a href="https://www.linkedin.com/in/shadrack-kioko-b2a712179/" className="icon i-YoutubeIcon">
+                        <LinkedIn />
+                    </a>
+                </div>
+            </div>
+        </HomePageStyled>
+    )
+}
+
+const HomePageStyled = styled.header`
+    width: 100%;
+    height: 100vh;
+    position: relative;
+   
+    .typography{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        width: 80%;
+
+        .icons{
+            display: flex;
+            justify-content: center;
+            margin-top: 1rem;
+            .icon{
+                border: 2px solid var(--border-color);
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                border-radius: 50%;
+                transition: all .4s ease-in-out;
+                cursor: pointer;
+                &:hover{
+                    border: 2px solid var(--primary-color);
+                    color: var(--primary-color);
+                }
+                &:not(:last-child){
+                    margin-right: 1rem;
+                }
+                svg{
+                    margin: .5rem;
+                }
+            }
+
+            .i-youtube{
+                &:hover{
+                    border: 2px solid red;
+                    color: red;
+                }
+            }
+            .i-github{
+                &:hover{
+                    border: 2px solid #5F4687;
+                    color: #5F4687;
+                }
+            }
+        }
+    }
+`;
+
+export default HomePage;
